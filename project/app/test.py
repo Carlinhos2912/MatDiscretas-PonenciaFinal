c22bdd03036677f341ae41fab4f3964da38ad8fe
mariaher05th <aferrerj@uninorte.edu.co>
2024-11-16 21:24:26 -0500

    Unified JSON to graph

--- project/app/test.py
+++ project/app/test.py
@@ -1,6 +1,18 @@
 from models.json_filedata import JSON_FileData
+from models.graph import Graph
 
 if __name__ == "__main__":
     
-    #json test
-    filedata = JSON_FileData("project/app/static/resources/test.json")
+    # JSON test
+    filedata = JSON_FileData("project/app/static/resources/airports_data.json")
+    grafito = Graph(len(filedata.code_list))
+    
+    for flight in filedata.connections:
+        grafito.add_edge ( 
+                         filedata.code_dict[flight[0]],     # Source code to index 
+                         filedata.code_dict[flight[1]],     # Destination code to index
+                         flight[2]                          # Distance
+                         )
+    
+    for index in range(grafito.size):
+        print(f"{filedata.code_list[index]} -> {grafito.adjacency[index]}" )
