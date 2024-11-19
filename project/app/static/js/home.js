12a5f6de9ee96eb7932713c062c7a3db16194d41
💫 Lic. Avila <aferrerj@uninorte.edu.co>
2024-11-18 23:02:14 -0500

    ya hice push

--- project/app/static/js/home.js
+++ project/app/static/js/home.js
@@ -9,11 +9,16 @@
 
 var map;
 var markersMap = {};
+let isSelecting = false;
 let currentMode = 'full';
 
 let airports = {};
 let pathData = [];
 let mstData = [];
+let curveLayers = [];
+
+const sourceCodeHolder = document.getElementById('read-id-1');
+const destinationCodeHolder = document.getElementById('read-id-2');
 
 /* ====================================================
                      Punto de entrada
@@ -26,16 +31,22 @@ window.onload = setup;
     ==================================================== */
 function setup() {
     document.getElementById('loader').style.display = 'flex';
-    document.getElementById('read-id-1').addEventListener('input' , () => { } );
-    document.getElementById('read-id-2').addEventListener('input' , () => { } );
     document.getElementById('fetch-data').addEventListener('click', () => { } );
+    
+    sourceCodeHolder.addEventListener('input' , () => { } );
+    destinationCodeHolder.addEventListener('input' , () => { } );
 
     initMap();
     initAirportData();
+
 }
 
 function initMap() {
-    map = L.map('map').setView([4.5709, -74.2973], 6);
+    map = L.map('map', {
+        fadeAnimation: false, 
+    })
+    .setView([4.5709, -74.2973], 6);
+    
     var bounds = [[-4.2316872, -82.1243666], [13.3920668, -66.8471272]];
 
     map.setMaxBounds(bounds);
@@ -60,6 +71,7 @@ function initAirportData(jsonPath = '../static/resources/extended_airports_data.
     })
     .then(() => {
         initAirportsMarkers()
+        updateData();
     })
     .catch((error) => {
         console.error('Error fetching the JSON file:', error);
@@ -81,46 +93,88 @@ function createMarker(latitude, longitude, airportCode) {
     markersMap[airportCode] = marker;
     marker.addTo(map);
 
-+    marker.on('click', function () {
-        map.flyTo([latitude, longitude], 10);
+    let isMouseInside = false;
+
+    marker.on('click', function () { 
+        map.flyTo([latitude, longitude], 10); 
+        
+        sourceCodeHolder.value = airportCode
+        document.getElementById("source-city").textContent = airports[airportCode].name
+        document.getElementById("source-country").textContent = airports[airportCode].city
+
     });
 
+    marker.on('contextmenu', function (e) {
+        console.log("Marker right-clicked");
+    
+        const contextMenu = document.getElementById("custom-menu");
+    
+        contextMenu.style.left = `${e.originalEvent.pageX}px`; 
+        contextMenu.style.top = `${e.originalEvent.pageY}px`;
+    
+        contextMenu.classList.remove("hidden");
+    
+        e.originalEvent.preventDefault();
+    });
+    
+
     marker.on('mouseover', function () {
-        const popupContent = `
-            <b><span>${airportCode} | </span>${airports[airportCode].name}</b>
-        `;
-        marker.bindPopup(popupContent).openPopup(); 
-        highlightAdjacentMarkers(airportCode, 0.4); 
+        if (!isMouseInside) {
+            isMouseInside = true; 
+
+            const popupContent = `
+                <b><span>${airportCode} | </span>${airports[airportCode].name}</b>
+            `;
+            marker.bindPopup(popupContent).openPopup(); 
+            highlightAdjacentMarkers(airportCode, 0.4); 
+        }
     });
 
     marker.on('mouseout', function () {
-        marker.closePopup(); 
+        isMouseInside = false; 
+        marker.closePopup();
         highlightAdjacentMarkers(); 
+        curveLayers.forEach(curve => map.removeLayer(curve));
+        curveLayers = [];
     });
 
     return marker;
 }
 
-
 function highlightAdjacentMarkers(airportCode = null, opacity = 1) {
     for (let key in markersMap) {
-        markersMap[key].setIcon(createMarkerIcon(false, opacity));
+        const markerElement = markersMap[key].getElement(); 
+        if (markersMap[key].getElement()) {
+            markerElement.style.transition = 'opacity 0.3s ease';
+            markerElement.style.opacity = opacity; 
+            markersMap[key].setIcon(createMarkerIcon(false));
+        }
     }
 
     if (airportCode) {
         airports[airportCode].connections.forEach(connectionCode => {
             if (markersMap[connectionCode]) {
-                markersMap[connectionCode].setIcon(createMarkerIcon(true)); // Active icon
+                markersMap[connectionCode].getElement().style.opacity = '1'; 
+                markersMap[connectionCode].setIcon(createMarkerIcon(true));
+            }
+
+            if (airports[connectionCode]) {
+                const curve = createCurve(
+                                            [airports[airportCode].latitude, airports[airportCode].longitude], 
+                                            [airports[connectionCode].latitude, airports[connectionCode].longitude]);
+                curve.addTo(map);
+                curveLayers.push(curve);
             }
         });
 
-        if (markersMap[airportCode]) {
-            markersMap[airportCode].setIcon(createMarkerIcon(true)); // Active icon
+        if ( markersMap[airportCode]) {
+            markersMap[airportCode].getElement().style.opacity = '1'; 
+            markersMap[airportCode].setIcon(createMarkerIcon(true));
         }
+
+
     }
 }
-
-
 
 /* ==================================
                Utilidades 
@@ -134,25 +188,37 @@ function createMarkerIcon(isActive = false, opacity = 1) {
     });
 }
 
+function createCurve(start, end) {
+    const coords = [
+        'M', [start[0], start[1]],
+        'Q',
+        [ (start[0] + end[0]) / 2 + 1, (start[1] + end[1]) / 2 + 1 ], 
+        [end[0], end[1]] 
+    ];
 
-/*
-function coordsToPixel(latlng) {
-    const point = map.latLngToContainerPoint(latlng);
-    return { x: point.x , y: point.y};
+    return L.curve(coords, {
+        color: '#FF5733',
+        weight: 3,
+        opacity: 0.8,
+        dashArray: '5,5' 
+    });
 }
 
-function displayContextMenu(latlng, airport){
-    const contextMenu = document.getElementById('info-context-menu');
-    if ( contextMenu ) contextMenu.remove();
-    
-    const infoContextMenu = document.createElement('div');
-    infoContextMenu.id = 'info-context-menu';
-    console.log(airport)
-    infoContextMenu.textContent = airport["name"];
+function updateData(){
+    document.getElementById("source-city").textContent = airports[sourceCodeHolder.value].name
+    document.getElementById("source-country").textContent = airports[sourceCodeHolder.value].city
+    document.querySelector("#latitude-source h2").textContent = airports[sourceCodeHolder.value].latitude
+    document.querySelector("#longitude-source h2").textContent = airports[sourceCodeHolder.value].longitude
 
-    infoContextMenu.style.left = `calc(${coordsToPixel(latlng).x}px + 25vw)`;
-    infoContextMenu.style.top = coordsToPixel(latlng).y + 'px';
-    
-    document.body.appendChild(infoContextMenu);
+
+    document.getElementById("destination-city").textContent = airports[destinationCodeHolder.value].name
+    document.getElementById("destination-country").textContent = airports[destinationCodeHolder.value].city
+    document.querySelector("#latitude-destination h2").textContent = airports[destinationCodeHolder.value].latitude
+    document.querySelector("#longitude-destination h2").textContent = airports[destinationCodeHolder.value].longitude
 }
-    */
+
+
+/*  
+    Menu Custom para realizar acciones
+*/
+
